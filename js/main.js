2a3a54bf2888df5b95603d7f64d63f6a587551e0
Kelvin Lau <kelvin@infinityloop.ca>
2014-02-08 18:03:51 -0500

    Added in skills. There is a bug with isotope where it over laps skills section.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,122 @@
 $(document).ready(function() {
-
+    // Initialize the isotop project area
+    $('#projectcontainer').isotope({
+        itemSelector: '.projectsquare',
+        sortBy: 'random',
+        masonry: {
+            columnWidth: 10
+         }
+    });
     
+    // Raphael Skill Circle
+    var archtype = Raphael("skillsCanvas", 400, 400);
+    archtype.customAttributes.arc = function (xloc, yloc, value, total, R) {
+        var alpha = 360 / total * value,
+            a = (90 - alpha) * Math.PI / 180,
+            x = xloc + R * Math.cos(a),
+            y = yloc - R * Math.sin(a),
+            path;
+        if (total == value) {
+            path = [
+                ["M", xloc, yloc - R],
+                ["A", R, R, 0, 1, 1, xloc - 0.01, yloc - R]
+            ];
+        } else {
+            path = [
+                ["M", xloc, yloc - R],
+                ["A", R, R, 0, +(alpha > 180), 1, x, y]
+            ];
+        }
+        return {
+            path: path
+        };
+    };
+    
+    //make an arc at 50,50 with a radius of 30 that grows from 0 to 40 of 100 with a linear
+    var php_arc = archtype.path().attr({
+        "stroke": "#99c",
+        "stroke-width": 16,
+        arc: [200, 200, 0, 100, 30]
+    });
+
+    var html_arc = archtype.path().attr({
+        "stroke": "#f51",
+        "stroke-width": 16,
+        arc: [200, 200, 0, 100, 20]
+    });
+    
+     var jquery_arc = archtype.path().attr({
+        "stroke": "#0769AD",
+        "stroke-width": 16,
+        arc: [200, 200, 0, 100, 20]
+    });
+     var css_arc = archtype.path().attr({
+        "stroke": "#8AC007",
+        "stroke-width": 16,
+        arc: [200, 200, 0, 100, 20]
+    });
+    
+     var sql_arc = archtype.path().attr({
+        "stroke": "#FE0",
+        "stroke-width": 16,
+        arc: [200, 200, 0, 100, 20]
+    });
+    
+     var csharp_arc = archtype.path().attr({
+        "stroke": "#68217a",
+        "stroke-width": 16,
+        arc: [200, 200, 0, 100, 20]
+    });
+    
+     var java_arc = archtype.path().attr({
+        "stroke": "#DE0000",
+        "stroke-width": 16,
+        arc: [200, 200, 0, 100, 20]
+    });
+    
+     var java_arc = archtype.path().attr({
+        "stroke": "#DE0000",
+        "stroke-width": 16,
+        arc: [200, 200, 0, 100, 20]
+    });
+    
+     var c_arc = archtype.path().attr({
+        "stroke": "#99C0FF",
+        "stroke-width": 16,
+        arc: [200, 200, 0, 100, 20]
+    });
+    
+    php_arc.animate({
+        arc: [200, 200, 76, 100, 170]
+    }, 1500, "linear");
+
+    html_arc.animate({
+        arc: [200, 200, 68, 100, 150]
+    }, 1500, "linear");
+    
+     jquery_arc.animate({
+        arc: [200, 200, 68, 100, 130]
+    }, 1500, "linear");
+    
+     css_arc.animate({
+        arc: [200, 200, 62, 100, 110]
+    }, 1500, "linear");
+    
+     sql_arc.animate({
+        arc: [200, 200, 54, 100, 90]
+    }, 1500, "linear");
+    
+     csharp_arc.animate({
+        arc: [200, 200, 42, 100, 70]
+    }, 1500, "linear");
+    
+     java_arc.animate({
+        arc: [200, 200, 35, 100, 50]
+    }, 1500, "linear");
+    
+     c_arc.animate({
+        arc: [200, 200, 32, 100, 30]
+    }, 1500, "linear");
 });
  
  var showProjects = true;
@@ -19,27 +135,15 @@ $(window).scroll(function() {
         $('#quote1author').fadeOut();
     }
     
-    if(($('#projects').position().top - scrollBottom) <= 0 && showProjects == true) {
 
-        var $container = $('#projectcontainer');
-        $container.fadeIn(2000);
-        $container.isotope({
-            itemSelector: '.projectsquare',
-            sortBy: 'random',
-            masonry: {
-                columnWidth: 10
-             }
-        });
-                               
-        showProjects = false;
-    }
     //console.log($('#quote1').innerHeight());
     //console.log($('#quote1')[0].scrollHeight);
     //console.log('projectDiv: ' + ($('#projects').position().top - ($(document).scrollTop() + offset * 2)));
     //console.log('offset: ' + offset);
-    console.log($('#projects').position().top);
-    var scrollBottom = $(window).scrollTop() + $(window).height();
-    console.log($('#projects').position().top - scrollBottom);
+    
+    //console.log($('#projects').position().top);
+    //var scrollBottom = $(window).scrollTop() + $(window).height();
+    //console.log($('#projects').position().top - scrollBottom);
 });
 
 
