ac9b55e60235aa5b1d9845329acd7c4f0972a369
Kelvin Lau <infinityloop@hotmail.ca>
2014-02-09 00:56:52 -0500

    Completed Skills section

--- js/main.js
+++ js/main.js
@@ -8,6 +8,43 @@ $(document).ready(function() {
          }
     });
     
+});
+ 
+ var showSkills = true;
+ 
+ // Scroll Listener
+$(window).scroll(function() {
+        
+    if(atActivationPoint($('#quote1'))) {
+        $('#quote1text').fadeIn();
+        $('#quote1author').fadeIn();
+        
+    } else {
+        $('#quote1text').fadeOut();
+        $('#quote1author').fadeOut();
+    }
+    
+    if(atActivationPoint($('#skills')) && showSkills == true) {
+        activateSkills();
+        showSkills = false;
+    }
+
+    //console.log($('#quote1').innerHeight());
+    //console.log($('#quote1')[0].scrollHeight);
+    //console.log('projectDiv: ' + ($('#projects').position().top - ($(document).scrollTop() + offset * 2)));
+    //console.log('offset: ' + offset);
+    
+    //console.log($('#projects').position().top);
+    //var scrollBottom = $(window).scrollTop() + $(window).height();
+    //console.log($('#projects').position().top - scrollBottom);
+});
+
+function atActivationPoint($item) {
+    var offset = $(window).height()/ 2.5;
+    return (($item.position().top - ($(document).scrollTop() + offset)) <= 0)
+}
+
+function activateSkills() {
     // Raphael Skill Circle
     var archtype = Raphael("skillsCanvas", 400, 400);
     archtype.customAttributes.arc = function (xloc, yloc, value, total, R) {
@@ -32,119 +69,99 @@ $(document).ready(function() {
         };
     };
     
-    //make an arc at 50,50 with a radius of 30 that grows from 0 to 40 of 100 with a linear
-    var php_arc = archtype.path().attr({
+    var php_arc;
+    var html_arc;
+    var jquery_arc;
+    var css_arc;
+    var sql_arc;
+    var csharp_arc;
+    var java_arc;
+    var c_arc;
+    
+    //make an arc at 200,200 with a radius of 30 that grows from 0 to X of 100 with a linear
+    php_arc = archtype.path().attr({       
         "stroke": "#99c",
         "stroke-width": 16,
         arc: [200, 200, 0, 100, 30]
     });
 
-    var html_arc = archtype.path().attr({
-        "stroke": "#f51",
+    html_arc = archtype.path().attr({
+        "stroke": "#e34a22",
         "stroke-width": 16,
         arc: [200, 200, 0, 100, 20]
     });
     
-     var jquery_arc = archtype.path().attr({
+    jquery_arc = archtype.path().attr({
         "stroke": "#0769AD",
         "stroke-width": 16,
         arc: [200, 200, 0, 100, 20]
     });
-     var css_arc = archtype.path().attr({
+    
+    css_arc = archtype.path().attr({
         "stroke": "#8AC007",
         "stroke-width": 16,
         arc: [200, 200, 0, 100, 20]
     });
     
-     var sql_arc = archtype.path().attr({
-        "stroke": "#FE0",
+    sql_arc = archtype.path().attr({
+        "stroke": "#e97b00",
         "stroke-width": 16,
         arc: [200, 200, 0, 100, 20]
     });
     
-     var csharp_arc = archtype.path().attr({
+    csharp_arc = archtype.path().attr({
         "stroke": "#68217a",
         "stroke-width": 16,
         arc: [200, 200, 0, 100, 20]
     });
     
-     var java_arc = archtype.path().attr({
+    java_arc = archtype.path().attr({
         "stroke": "#DE0000",
         "stroke-width": 16,
         arc: [200, 200, 0, 100, 20]
     });
     
-     var java_arc = archtype.path().attr({
-        "stroke": "#DE0000",
-        "stroke-width": 16,
-        arc: [200, 200, 0, 100, 20]
-    });
-    
-     var c_arc = archtype.path().attr({
+    c_arc = archtype.path().attr({
         "stroke": "#99C0FF",
         "stroke-width": 16,
         arc: [200, 200, 0, 100, 20]
     });
     
+    
     php_arc.animate({
         arc: [200, 200, 76, 100, 170]
-    }, 1500, "linear");
+    }, 1700, "linear");
 
     html_arc.animate({
         arc: [200, 200, 68, 100, 150]
-    }, 1500, "linear");
+    }, 1550, "linear");
     
-     jquery_arc.animate({
+    jquery_arc.animate({
         arc: [200, 200, 68, 100, 130]
-    }, 1500, "linear");
+    }, 1550, "linear");
     
-     css_arc.animate({
+    css_arc.animate({
         arc: [200, 200, 62, 100, 110]
-    }, 1500, "linear");
+    }, 1300, "linear");
     
-     sql_arc.animate({
+    sql_arc.animate({
         arc: [200, 200, 54, 100, 90]
-    }, 1500, "linear");
+    }, 1150, "linear");
     
-     csharp_arc.animate({
+    csharp_arc.animate({
         arc: [200, 200, 42, 100, 70]
-    }, 1500, "linear");
+    }, 1000, "linear");
     
-     java_arc.animate({
+    java_arc.animate({
         arc: [200, 200, 35, 100, 50]
-    }, 1500, "linear");
+    }, 850, "linear");
     
-     c_arc.animate({
+    c_arc.animate({
         arc: [200, 200, 32, 100, 30]
-    }, 1500, "linear");
-});
- 
- var showProjects = true;
- 
- // Scroll Listener
-$(window).scroll(function() {
-    var offset = $(window).height()/ 2.5;
-    var scrollBottom = $(window).scrollTop() + $(window).height();
-        
-    if(($('#quote1').position().top - ($(document).scrollTop() + offset)) <= 0 ) {
-        $('#quote1text').fadeIn();
-        $('#quote1author').fadeIn();
-        
-    } else {
-        $('#quote1text').fadeOut();
-        $('#quote1author').fadeOut();
-    }
+    }, 700, "linear");
     
-
-    //console.log($('#quote1').innerHeight());
-    //console.log($('#quote1')[0].scrollHeight);
-    //console.log('projectDiv: ' + ($('#projects').position().top - ($(document).scrollTop() + offset * 2)));
-    //console.log('offset: ' + offset);
-    
-    //console.log($('#projects').position().top);
-    //var scrollBottom = $(window).scrollTop() + $(window).height();
-    //console.log($('#projects').position().top - scrollBottom);
-});
+    $('.skillBlock').fadeIn(1700);
+}
 
 
 function sendGmail() {
